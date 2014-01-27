2db4f75c2a2da14995156d632a2db424b223457d
iidre-eeepc <iidre-eeepc@asus-eeepc.(none)>
2014-01-27 16:06:10 +0100

    Starting from a new simplified Diagram

--- doc/StationMeteo.Diagram/StationMeteo.Diagram.py
+++ doc/StationMeteo.Diagram/StationMeteo.Diagram.py
@@ -1,22 +1,29 @@
-class Station :
+class Sensor :
+	'''
+https://www.google.fr/#q=NVARCHAR+encodage+mysql
+https://stackoverflow.com/questions/612430/when-must-we-use-nvarchar-nchar-instead-of-varchar-char-in-sql-servers
+
+Nvarchar ne sert que pour les utilisateurs MS-SQL. '''
 	def __init__(self) :
-		self.ser = SerialInput() # 
+		pass
+class Station :
+	'''(NULL)'''
+	def __init__(self) :
+		self.ser = serial.Serial() # 
 		self.parser = InputParser() # 
 		self.datab = DatabManager() # 
-		self.input = None # str
+		self.raw_received_meterings = "" # str
+		self.metering_quantity = 0 # int
+		self.last_meterings_list = list() # 
 		self.sensor_dict = dict('id': ,'name': ) # 
-		self.last_meterings_list = LastMeteringList() # 
 		pass
-	def __get_serial_input_content (self, ser) :
+	def _get_meterings_raw_data (self) :
 		# returns 
 		pass
-	def __parse (self, serial_input_content) :
+	def _parse_raw_data (self) :
 		# returns 
 		pass
-	def __store_meterings (self) :
-		# returns 
-		pass
-	def __connect_serial (self) :
+	def _store_meterings (self) :
 		# returns 
 		pass
 	def setup (self) :
@@ -36,14 +43,6 @@ http://docs.sqlalchemy.org/en/rel_0_8/orm/tutorial.html#adding-new-objects'''
 		self.metering = Metering() # 
 		self.Sensors = Sensors() # 
 		pass
-class Sensors :
-	'''
-https://www.google.fr/#q=NVARCHAR+encodage+mysql
-https://stackoverflow.com/questions/612430/when-must-we-use-nvarchar-nchar-instead-of-varchar-char-in-sql-servers
-
-Nvarchar ne sert que pour les utilisateurs MS-SQL. '''
-	def __init__(self) :
-		pass
 class Metering :
 	'''
 http://docs.sqlalchemy.org/en/rel_0_8/orm/tutorial.html#declare-a-mapping
@@ -53,36 +52,5 @@ http://docs.sqlalchemy.org/en/rel_0_8/orm/tutorial.html#declare-a-mapping
 <class 'sqlalchemy.ext.declarative.Base'>
 >>> 
 '''
-	def __init__(self) :
-		pass
-class /dev/tty :
-	def __init__(self) :
-		self.name = None # string
-		pass
-class SerialInput :
-	'''
->>> from serial import Serial
->>> Serial()
-Serial<id=0xb767eb6c, open=False>(port=None, baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=None, xonxoff=False, rtscts=False, dsrdtr=False)
->>> help(Serial())
->>> help(Serial.readline())
->>> help(Serial.readlines())
-'''
-	def __init__(self) :
-		self.port = '/dev/ttyUSB0' # 
-		self.baudrate = 115200 # int
-		pass
-	def readline (self) :
-		# returns str
-		pass
-class InputParser :
-	def __init__(self) :
-		self.serial_input_content = None # str
-		self.last_meterings_list = LastMeteringList() # 
-		pass
-	def parse (self, ) :
-		# returns 
-		pass
-class LastMeteringList :
 	def __init__(self) :
 		pass
