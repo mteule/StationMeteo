ea2c1465b3e7159a8f75939dcb53fc7869368065
iidre-eeepc <iidre-eeepc@asus-eeepc.(none)>
2014-01-27 16:31:24 +0100

    Retrieving the auto generated code.

--- station_meteo/datab.py
+++ station_meteo/datab.py
@@ -2,3 +2,14 @@
 # -*- coding: utf-8 -*-
 
 # Copyright (C) 2014 Mathias Teulé <mathias.te@googlemail.com>
+
+class DatabManager :
+    '''
+http://docs.sqlalchemy.org/en/rel_0_8/orm/tutorial.html#adding-new-objects'''
+    def __init__(self) :
+        self.logger = logging.getLogger(__name__) # 
+        self.engine_url = 'sqlite:///:memory:' # str
+        self.engine = sqlalchemy.create_engine(engine_url, echo = True) # 
+        self.Session = sqlalchemy.orm.sessionmaker(bind=engine) # 
+        self.session = Session() # 
+        pass
--- station_meteo/station.py
+++ station_meteo/station.py
@@ -3,3 +3,30 @@
 
 # Copyright (C) 2014 Mathias Teulé <mathias.te@googlemail.com>
 
+class Station :
+    '''(NULL)'''
+    def __init__(self) :
+        self.logger = logging.getLogger(__name__) # 
+        self.ser = serial.Serial() # 
+        self.parser = InputParser() # 
+        self.datab = DatabManager() # 
+        self.raw_received_meterings = "" # str
+        self.metering_quantity = 0 # int
+        self.last_meterings_list = list() # 
+        self.sensor_dict = dict('id': ,'name': ) # 
+        pass
+    def _get_meterings_raw_data (self) :
+        # returns 
+        pass
+    def _parse_raw_data (self) :
+        # returns 
+        pass
+    def _store_meterings (self) :
+        # returns 
+        pass
+    def setup (self) :
+        # returns 
+        pass
+    def loop (self) :
+        # returns 
+        pass
