7c7987579a4119a856d8cf2ec201d26099e8a8e0
iidre-eeepc <iidre-eeepc@asus-eeepc.(none)>
2014-01-27 17:01:46 +0100

    Beginning

--- station_meteo/station.py
+++ station_meteo/station.py
@@ -3,30 +3,62 @@
 
 # Copyright (C) 2014 Mathias Teulé <mathias.te@googlemail.com>
 
-class Station :
+import logging
+import serial
+import sqlalchemy
+
+class Station (object):
     '''(NULL)'''
     def __init__(self) :
         self.logger = logging.getLogger(__name__) # 
         self.ser = serial.Serial() # 
-        self.parser = InputParser() # 
         self.datab = DatabManager() # 
         self.raw_received_meterings = "" # str
         self.metering_quantity = 0 # int
         self.last_meterings_list = list() # 
         self.sensor_dict = dict('id': ,'name': ) # 
         pass
-    def _get_meterings_raw_data (self) :
-        # returns 
+
+    def setup (self) :
+        self.ser.port = '/dev/ttyUSB0'
+        self.ser.baudrate = 115200
+        self.ser.open()
         pass
+        
+    def loop (self) :
+        if  _get_meterings_raw_data():
+             # refresh datab and sensor_dict:
+             datab.session.connect #?
+             self.sensor_dict = 
+             
+             _parse_raw_data()
+             
+             for metering in self.last_meterings_list:
+                 try:
+                     datab.session.add(metering)
+                 except Error() as err:
+                     self.logger.ERROR(err)
+        pass
+
+    def _get_meterings_raw_data (self) :
+        self.raw_received_meterings = self.ser.readline()
+        if not 0==len(self.raw_received_meterings)
+            return True
+        else:
+            return False
+        pass
+
     def _parse_raw_data (self) :
+        self.raw_received_meterings = "" # str
+        self.metering_quantity = 0 # int
+        self.last_meterings_list = list() # 
         # returns 
         pass
     def _store_meterings (self) :
-        # returns 
+         for metering in self.last_meterings_list:
+             try:
+                 datab.session.add(metering)
+             except Error() as err:
+                 self.logger.ERROR(err)
         pass
-    def setup (self) :
-        # returns 
-        pass
-    def loop (self) :
-        # returns 
-        pass
+
