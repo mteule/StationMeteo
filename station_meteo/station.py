825fc727f7db05209aa536982715dc38caf33c4a
Putty <putty@Debian-Lenovo-Putty>
2014-02-03 18:29:27 +0100

    encapsulate ._parse_raw_data()

--- station_meteo/station.py
+++ station_meteo/station.py
@@ -15,7 +15,8 @@ class Station (object):
         self.logger = logging.getLogger(__name__) # 
         self.ser = serial.Serial() # 
         self.datab = DatabManager() # 
-        self.raw_received_meterings = "" # str
+        self.raw_received_meterings = ("TEMP,-1,17.40,HUM,-1,57.50,NO2,4236,15.4445400238,CO,125283," +
+    "17411.0546875000,VOC,141338,22.7283306121,Dust,2776,0.0003270847\n\r") # str
         self.metering_quantity = 0 # int
         self.last_meterings_list = list() # 
         self.sensor_dict = dict({'id':"" ,'name':""}) # 
@@ -48,8 +49,8 @@ class Station (object):
             return False
         pass
 
-    def _parse_raw_data (self, raw_received_meterings=""):
-        data = raw_received_meterings.rstrip() 
+    def _parse_raw_data (self):
+        data = self.raw_received_meterings.rstrip() 
         split = [elem.strip() for elem in data.split(',')]
         metering_quantity = len(split) / 3
         # Check only the data won't get us into "pointer out of cast" troubles:
@@ -57,13 +58,12 @@ class Station (object):
         	raise StandartError("raw data is not consistent")
     	
         metering = dict({'name': 'some_sensor_name', 'raw': 0, 'value': 0})
-        last_meterings_list = list()
         for i in range (0, metering_quantity):
             metering ['name'] = split[(i*3 + 0)]
             metering ['raw'] = split[(i*3 + 1)]
             metering ['value'] = split[(i*3 + 2)]
-            last_meterings_list.append(metering)
-        return last_meterings_list
+            self.last_meterings_list.append(metering)
+        pass
 
     def _store_meterings (self) :
         for elem in self.last_meterings_list:
