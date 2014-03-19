2104357ee2f7efdd8c07d6b16cc270dceab597a0
iidre-eeepc <iidre-eeepc@asus-eeepc.(none)>
2014-03-19 04:18:04 +0100

    Add test_parse_raw_string(self)

--- tests/test_last_meterings.py
+++ tests/test_last_meterings.py
@@ -20,3 +20,21 @@ class Test_LastMeterings(unittest.TestCase):
         self.session = None
         self.last_meterings = None  # TODO: know if this is a convenient way
         pass
+
+    def test_parse_raw_string(self):
+        self.last_meterings.raw_string = (
+            "TEMP,-1,17.40,HUM,-1,57.50,NO2,4236,15.4445400238,CO,125283," +
+            "17411.0546875000,VOC,141338,22.7283306121,Dust,2776,0.0003270847" +
+            "\n\r")
+        a = [
+        {'raw': '-1', 'name': 'TEMP', 'value': '17.40'}, 
+        {'raw': '-1', 'name': 'HUM', 'value': '57.50'}, 
+        {'raw': '4236', 'name': 'NO2', 'value': '15.4445400238'}, 
+        {'raw': '125283', 'name': 'CO', 'value': '17411.0546875000'}, 
+        {'raw': '141338', 'name': 'VOC', 'value': '22.7283306121'}, 
+        {'raw': '2776', 'name': 'Dust', 'value': '0.0003270847'}]
+        self.station._parse_raw_data()
+        self.assertEqual(a, self.station.last_meterings_list)
+        pass
+
+
