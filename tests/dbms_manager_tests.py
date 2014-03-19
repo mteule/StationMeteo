a42d8cde79506fc26dbe32253289a51128e209de
iidre-eeepc <iidre-eeepc@asus-eeepc.(none)>
2014-03-19 03:29:30 +0100

    Add test_retrieve_sensor_id_dict(self): OK!

--- tests/dbms_manager_tests.py
+++ tests/dbms_manager_tests.py
@@ -26,3 +26,8 @@ class Test_DBMS_Manager(unittest.TestCase):
         self.dbms_manager = None  # TODO: know if this is a convenient way
         pass
 
+    def test_retrieve_sensor_id_dict(self):
+        a = {'CO': 3, 'TEMP': 1, 'VOC': 5, 'Dust': 6, 'HUM': 2, 'NO2': 4}
+        sensor_id_dict = self.dbms_manager.retrieve_sensor_id_dict()
+        self.assertEqual(a, sensor_id_dict)
+        pass 
