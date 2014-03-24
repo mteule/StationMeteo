bcaacc73d2e01e3777fd1fdaf9db0ed6cf9058f8
iidre-eeepc <iidre-eeepc@asus-eeepc.(none)>
2014-03-24 17:01:58 +0100

    Beginning to clean station.py

--- station_meteo/station.py
+++ station_meteo/station.py
@@ -23,13 +23,6 @@ import time
 import datetime
 import sqlalchemy
 
-from model import *
-
-# TODO: rm, moved to DBMS_Manager()
-# moved here to use a RAW 'sqlacodegen' generated model.py
-db_url = 'mysql://monty:passwd@localhost/test_dia'
-engine = sqlalchemy.create_engine(db_url)
-metadata.bind = engine
 
 import last_meterings
 import dbms_manager
@@ -42,32 +35,9 @@ class Station (object):
 
     logger = logging.getLogger(__name__)
 
-    clock = datetime.datetime
     ser = serial.Serial()
-
-    # The new classes:
     last_meterings = last_meterings.LastMeterings()
     dbms_manager = dbms_manager.DBMS_Manager()
-
-
-    # TODO: rm, moved to DBMS_Manager()
-    # DBMS connection:
-    sensor_table = Sensor().__table__
-    metering_table = Metering().__table__
-
-    # TODO: remove all this part when it is switched towards LastMeterings()
-    # "Serial Inputted/Parsed" datas
-    raw_received_meterings = ""  # str
-    last_meterings_list = list(
-            dict({
-                'name': 'some_sensor_name',
-                'raw': 0,
-                'value': 0}))
-    sensor_id_dict = dict({
-        'name_sensor_1': 'id == 1',
-        'name_sensor_2': 'id == 2',
-        'name_sensor_3': 'id == 3',
-        'name_sensor_4': 'id == 4'})
 
     def __init__(self):
         """"""
@@ -75,30 +45,6 @@ class Station (object):
         #        move here our setup of serial.Serial()
         pass
 
-    def loop(self):
-        """"""
-        self.ser.port = '/dev/ttyUSB0'
-        self.ser.baudrate = 115200
-        self.ser.open()
-
-        while True:
-            if not self._got_meterings_raw_data():
-                # must wait for a while
-                delay = 1  # seconds
-                time.sleep(delay)
-            else:
-                # store meterings
-                self._parse_raw_data()
-                self._append_clock(self.clock.now())
-                self._refresh_sensor_id_dict()
-                self._append_sensor_id()
-                for elem in self.last_meterings_list:
-                    try:
-                        self._insert_metering(elem)
-                    except sqlalchemy.exc.IntegrityError as err:
-                        # Surely raised if the Sensor table is incomplete
-                        self.logger.error(err)
-        pass
 
     def loop_new(self):
         """"""
@@ -126,40 +72,7 @@ class Station (object):
                         self.logger.error(err)
         pass
 
-    # TODO: rm, moved to DBMS_Manager()
-    def _refresh_sensor_id_dict(self):
-        """"""
-        # mysql> select id, bus_adress from Sensor
-        self.sensor_id_dict.clear()
-        sel = sqlalchemy.select([
-            self.sensor_table.c.bus_adress,
-            self.sensor_table.c.id])
-        res = sel.execute()
-        for row in res:
-            # Strange but 'id' is received as "type(id)==long"?!?
-            new_keyval = {
-                row[self.sensor_table.c.bus_adress]:
-                int(row[self.sensor_table.c.id])}
-            self.sensor_id_dict.update(new_keyval.copy())
-            # type(new_keyval) == dict() with only one key
-        pass
-
-    # TODO: rm, moved to LastMeterings()
-    def _append_clock(self, now):
-        """"""
-        new_keyval = {'date': now}
-        for metering_dict in self.last_meterings_list:
-            metering_dict.update(new_keyval)
-        pass
-
-    # TODO: rm, moved to LastMeterings()
-    def _append_sensor_id(self):
-        """"""
-        for metering_dict in self.last_meterings_list:
-            bus_adress = metering_dict['name']
-            new_keyval = {'sensor_id': self.sensor_id_dict[bus_adress]}
-            metering_dict.update(new_keyval.copy())
-        pass
+ 
 
     def _got_meterings_raw_data(self):
         """"""
@@ -171,60 +84,22 @@ class Station (object):
             found_new_line = False
         return found_new_line
 
-    # TODO: rm, moved to LastMeterings()
-    def _parse_raw_data(self):
-        """"""
-        del(self.last_meterings_list[:])
-        data = self.raw_received_meterings.rstrip()
-        split = [elem.strip() for elem in data.split(',')]
-        metering_quantity = len(split) / 3  # 3 params for each sensor
-
-        # Check only the data won't get us into "pointer out of cast" troubles:
-        if not (0 == len(split) % metering_quantity):
-            raise StandartError("raw data is not consistent")
-
-        # Construct self.last_meterings_list
-        metering = dict({'name': 'some_sensor_name', 'raw': 0, 'value': 0})
-        for i in range(metering_quantity):
-            metering['name'] = split[(i * 3 + 0)]
-            metering['raw'] = split[(i * 3 + 1)]
-            metering['value'] = split[(i * 3 + 2)]
-            self.last_meterings_list.append(metering.copy())
-
-        self.logger.debug("new values for self.last_meterings_list:\n"
-            + str(self.last_meterings_list))
-        pass
-    # TODO: rm, moved to DBMS_Manager()
-    def _insert_metering(self, meterings={}):
-        """"""
-        ins = self.metering_table.insert().values(
-            value=meterings['value'],
-            datetime=meterings['date'],
-            raw=meterings['raw'],
-            sensor_id=meterings['sensor_id'])
-        # .'compile().params' is usefull for debug,
-        #        but anyway it would be done automatically when executing
-        ins.compile().params
-        ins.execute()
-        # self.logger.debug(ins)
-pass
-
 if __name__ == "__main__":
         logging.basicConfig(level=logging.DEBUG)
         station = Station()
         station.logger.setLevel("DEBUG")
 
-        # insert metering value
-        station._insert_metering(
-            {'date': datetime.datetime(2014, 2, 26, 3, 10, 38, 371623),
-            'raw': '-1', 'sensor_id': 1, 'name': 'TEMP', 'value': '17.40'})
-
-        # Exception while inserting metering value
-        print ("Intending to commit a Foreign Key Constraint error:")
-        try:
-            station._insert_metering(
-                {'date': datetime.datetime(2014, 2, 26, 3, 10, 38, 371623),
-                'raw': '-1', 'sensor_id': 77, 'name': 'TEMP', 'value': '17.40'})
-        except sqlalchemy.exc.IntegrityError as err:
-            print(err)
+#        # insert metering value
+#        station._insert_metering(
+#            {'date': datetime.datetime(2014, 2, 26, 3, 10, 38, 371623),
+#            'raw': '-1', 'sensor_id': 1, 'name': 'TEMP', 'value': '17.40'})
+#
+#        # Exception while inserting metering value
+#        print ("Intending to commit a Foreign Key Constraint error:")
+#        try:
+#            station._insert_metering(
+#                {'date': datetime.datetime(2014, 2, 26, 3, 10, 38, 371623),
+#                'raw': '-1', 'sensor_id': 77, 'name': 'TEMP', 'value': '17.40'})
+#        except sqlalchemy.exc.IntegrityError as err:
+#            print(err)
 
