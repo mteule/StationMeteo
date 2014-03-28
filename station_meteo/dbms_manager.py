fe82ea98eb66f59eb865c879e0c4cf19037e139e
iidre-eeepc <iidre-eeepc@asus-eeepc.(none)>
2014-03-28 16:20:45 +0100

    Fix (almost) the ins_string value in debug log, but only as a dict()

--- station_meteo/dbms_manager.py
+++ station_meteo/dbms_manager.py
@@ -3,6 +3,7 @@
 
 # Copyright (C) 2014 Mathias Teulé <mathias.te@googlemail.com>
 
+import logging
 import sqlalchemy
 
 from model import Sensor, Metering, metadata
@@ -10,8 +11,8 @@ from model import Sensor, Metering, metadata
 """
 This dbms_manager module will be used to interact with databases.
 
-It is separated from, but uses, the sqlalchemy declarative model, 
-imported as model.py.
+It is separated from, but uses, the sqlalchemy declarative model,
+ imported as model.py.
 
 It initializes the globals values of the declarative model
 
@@ -27,6 +28,8 @@ class DBMS_Manager (object):
     """
     Standart object used to gather all the operation on the DBMS.
     """
+    logger = logging.getLogger(__name__)
+
     # DBMS connection:
     sensor_table = Sensor().__table__
     metering_table = Metering().__table__
@@ -35,12 +38,12 @@ class DBMS_Manager (object):
         """
         Retrieve a sensor_id_dict from the database.
         The reference of the dict is given as the return value.
-        
+
         It's content corresponds to a simple select, for example with mysql:
             mysql> select id, bus_adress from Sensor;
-            
+
         the returned dict has to look like this:
-        
+
         {'CO': 3, 'TEMP': 1, 'VOC': 5, 'Dust': 6, 'HUM': 2, 'NO2': 4}
         """
 
@@ -60,17 +63,17 @@ class DBMS_Manager (object):
 
     def insert_metering(self, meterings={}):
         """
-        Simply use the sqlalchemy DBMS connection 
+        Simply use the sqlalchemy DBMS connection
         to insert the values of a metering.
         The metering values are passed as a simple dict().
-        
+
         For example:
-            
-            {'date': datetime.datetime(2014, 2, 27, 5, 59, 28, 262085), 
+
+            {'date': datetime.datetime(2014, 2, 27, 5, 59, 28, 262085),
             'raw': '-1', 'sensor_id': 2, 'name': 'HUM', 'value': '57.50'}
-            
+
         The only keys that will be inserted will be:
-        
+
             meterings['value'],
             meterings['date'],
             meterings['raw'],
@@ -83,9 +86,29 @@ class DBMS_Manager (object):
             sensor_id=meterings['sensor_id'])
         # .'compile().params' is usefull for debug,
         #        but anyway it would be done automatically when executing
-        ins.compile().params
+        ins_string = ins.compile().params
         ins.execute()
-        # self.logger.debug(ins)
+        self.logger.debug(ins_string)
         pass
 pass
 
+if __name__ == "__main__":
+        import datetime
+
+        logging.basicConfig(level=logging.DEBUG)
+        dbms_manager = DBMS_Manager()
+        dbms_manager.logger.setLevel("DEBUG")
+
+        # functionnal test:
+        metering = {
+            'date': datetime.datetime(2014, 2, 26, 3, 10, 38, 371623),
+            'raw': '-1', 'sensor_id': 1, 'name': 'TEMP', 'value': '17.40'}
+
+        dbms_manager.insert_metering(metering)
+
+        # Raising an Error: #TODO: Maybe needs another function.
+        metering = {  # wrong sensor id!
+            'date': datetime.datetime(2014, 2, 26, 3, 10, 38, 371623),
+            'raw': '-1', 'sensor_id': 77, 'name': 'TEMP', 'value': '17.40'}
+
+        dbms_manager.insert_metering(metering)
